5c193b932189c206980048edcbd56334017f62d0
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-07-22 11:30:47 +0100

    - calculate custom quote - refactor HTML markup

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -6,38 +6,38 @@
 // $(document).ready(() => {
 // })
 
-const run = () => {
-  // select all form inputs
-  const generalCPPJ = document.querySelector('#general-cleaning_people_per_job');
-  const generalCHFJ = document.querySelector('#general-cleaning_hours_for_job');
-  const generalCPPH = document.querySelector('#general-cleaning_price_per_hour');
-  const generalCM = document.querySelector('#general-cleaning_mensuration');
-  const generalCCPM = document.querySelector('#general-cleaning_cost_per_measurement');
-  const generalCD = document.querySelector('#general-cleaning_discount');
+// const run = () => {
+//   // select all form inputs
+//   const generalCPPJ = document.querySelector('#general-cleaning_people_per_job');
+//   const generalCHFJ = document.querySelector('#general-cleaning_hours_for_job');
+//   const generalCPPH = document.querySelector('#general-cleaning_price_per_hour');
+//   const generalCM = document.querySelector('#general-cleaning_mensuration');
+//   const generalCCPM = document.querySelector('#general-cleaning_cost_per_measurement');
+//   const generalCD = document.querySelector('#general-cleaning_discount');
 
-  const windowCPPJ = document.querySelector('#window-cleaning_people_per_job');
-  const windowCHFJ = document.querySelector('#window-cleaning_hours_for_job');
-  const windowCPPH = document.querySelector('#window-cleaning_price_per_hour');
-  const windowCM = document.querySelector('#window-cleaning_mensuration');
-  const windowCCPM = document.querySelector('#window-cleaning_cost_per_measurement');
-  const windowCD = document.querySelector('#window-cleaning_discount');
+//   const windowCPPJ = document.querySelector('#window-cleaning_people_per_job');
+//   const windowCHFJ = document.querySelector('#window-cleaning_hours_for_job');
+//   const windowCPPH = document.querySelector('#window-cleaning_price_per_hour');
+//   const windowCM = document.querySelector('#window-cleaning_mensuration');
+//   const windowCCPM = document.querySelector('#window-cleaning_cost_per_measurement');
+//   const windowCD = document.querySelector('#window-cleaning_discount');
 
-  const contractSPPJ = document.querySelector('#contract-service_people_per_job');
-  const contractSHFJ = document.querySelector('#contract-service_hours_for_job');
-  const contractSPPH = document.querySelector('#contract-service_price_per_hour');
-  const contractSM = document.querySelector('#contract-service_mensuration');
-  const contractSCPM = document.querySelector('#contract-service_cost_per_measurement');
-  const contractSD = document.querySelector('#contract-service_discount');
-  const contractMV = document.querySelector('#monthly_visit');
-  const contractPPV = document.querySelector('#price_per_visit');
-  const contractCPM = document.querySelector('#cost_per_month');
-  const contractCM = document.querySelector('#contract_months');
+//   const contractSPPJ = document.querySelector('#contract-service_people_per_job');
+//   const contractSHFJ = document.querySelector('#contract-service_hours_for_job');
+//   const contractSPPH = document.querySelector('#contract-service_price_per_hour');
+//   const contractSM = document.querySelector('#contract-service_mensuration');
+//   const contractSCPM = document.querySelector('#contract-service_cost_per_measurement');
+//   const contractSD = document.querySelector('#contract-service_discount');
+//   const contractMV = document.querySelector('#monthly_visit');
+//   const contractPPV = document.querySelector('#price_per_visit');
+//   const contractCPM = document.querySelector('#cost_per_month');
+//   const contractCM = document.querySelector('#contract_months');
 
-  const firstTimeCharge = document.querySelector('#first_time_charge');
-  const extraCleanCharge = document.querySelector('#extra_clean_charge');
-  const refrigeratorCharge = document.querySelector('#refrigerator_charge');
-  const ovenCleanCharge = document.querySelector('#oven_clean_charge');
-  const sumTotal = document.querySelector('.total');
-}
+//   const firstTimeCharge = document.querySelector('#first_time_charge');
+//   const extraCleanCharge = document.querySelector('#extra_clean_charge');
+//   const refrigeratorCharge = document.querySelector('#refrigerator_charge');
+//   const ovenCleanCharge = document.querySelector('#oven_clean_charge');
+//   const sumTotal = document.querySelector('.total');
+// }
 
-window.onload = run;
+// window.onload = run;
